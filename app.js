622096f138008435474759c3fa4bfc8794e9a4f1
aadhi-11 <adithyadasaadhiii@gmail.com>
2025-01-29 16:42:02 +0530

    unhandled routes response

--- app.js
+++ app.js
@@ -6,16 +6,24 @@ const tourRouter = require('./routes/tourRoutes')
 const userRouter = require('./routes/userRoutes')
 
 //1.Middlewares
+
 // if(process.env.NODE_ENV === 'development'){
 //     app.use(morgan('dev'));
 // }
 
 app.use(express.json());
-app.use(express.static(`${__dirname}/public`))
+app.use(express.static(`${__dirname}/public`));
 
 //2.routers
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*',(req, res, next) => {
+    res.status(404).json({
+        staus:'fail',
+        message:`Can't find  ${req.originalUrl} on this Server!`
+    })
+})
 
 module.exports = app;
 
